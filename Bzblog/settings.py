4ae64a40d760e9507faa15c2bf096a786a0f104d
B-zerk <anthonytayo155@gmail.com>
2024-07-17 13:59:56 +0100

    added cors cofig

--- Bzblog/settings.py
+++ Bzblog/settings.py
@@ -127,10 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    
-    "http://localhost:5173",
-]
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS').split(" ")
 
 
 
